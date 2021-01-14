fa228b93f89d4e73d89fbb12a42b53f6ed1bcfc9
shinya <shinya@example.com>
2021-01-14 12:41:11 +0900

    refactor imple code

--- app/RomanNumeralsTDD/DefaultRomanNumeralsConverter.swift
+++ app/RomanNumeralsTDD/DefaultRomanNumeralsConverter.swift
@@ -3,73 +3,40 @@ protocol RomanNumeralsConverter {
 }
 
 struct DefaultRomanNumeralsConverter: RomanNumeralsConverter {
+
+    private struct RomanNumeral {
+        let number: Int
+        let romanLetter: String
+    }
+
+    private let romanNumerals = [
+        RomanNumeral(number: 1000, romanLetter: "M"),
+        RomanNumeral(number: 900, romanLetter: "CM"),
+        RomanNumeral(number: 500, romanLetter: "D"),
+        RomanNumeral(number: 400, romanLetter: "CD"),
+
+        RomanNumeral(number: 100, romanLetter: "C"),
+        RomanNumeral(number: 90, romanLetter: "XC"),
+        RomanNumeral(number: 50, romanLetter: "L"),
+        RomanNumeral(number: 40, romanLetter: "XL"),
+
+        RomanNumeral(number: 10, romanLetter: "X"),
+        RomanNumeral(number: 9, romanLetter: "IX"),
+        RomanNumeral(number: 5, romanLetter: "V"),
+        RomanNumeral(number: 4, romanLetter: "IV"),
+
+        RomanNumeral(number: 1, romanLetter: "I")
+    ]
+
     func convert(_ number: Int) -> String {
         var workingNumber = number
         var result = ""
 
-        while workingNumber >= 1000 {
-            result += "M"
-            workingNumber -= 1000
-        }
-
-        while workingNumber >= 900 {
-            result += "CM"
-            workingNumber -= 900
-        }
-        
-        while workingNumber >= 500 {
-            result += "D"
-            workingNumber -= 500
-        }
-
-        while workingNumber >= 400 {
-            result += "CD"
-            workingNumber -= 400
-        }
-
-        while workingNumber >= 100 {
-            result += "C"
-            workingNumber -= 100
-        }
-
-        while workingNumber >= 90 {
-            result += "XC"
-            workingNumber -= 90
-        }
-
-        while workingNumber >= 50 {
-            result += "L"
-            workingNumber -= 50
-        }
-
-        while workingNumber >= 40 {
-            result += "XL"
-            workingNumber -= 40
-        }
-        
-        while workingNumber >= 10 {
-            result += "X"
-            workingNumber -= 10
-        }
-
-        while workingNumber >= 9 {
-            result += "IX"
-            workingNumber -= 9
-        }
-
-        while workingNumber >= 5 {
-            result += "V"
-            workingNumber -= 5
-        }
-
-        while workingNumber >= 4 {
-            result += "IV"
-            workingNumber -= 4
-        }
-
-        while workingNumber > 0 {
-            result += "I"
-            workingNumber -= 1
+        romanNumerals.forEach { romanNumeral in
+            while workingNumber >= romanNumeral.number {
+                result += romanNumeral.romanLetter
+                workingNumber -= romanNumeral.number
+            }
         }
 
         return result
